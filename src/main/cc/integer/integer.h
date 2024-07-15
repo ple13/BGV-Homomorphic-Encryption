f14b44f11dc3245051e3a36bf4db25823007e0a5
ple13 <phihung.bk03@gmail.com>
2024-07-15 00:06:30 -0400

    Add integer class.

--- src/main/cc/integer/integer.h
+++ src/main/cc/integer/integer.h
@@ -12,3 +12,30 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+namespace integer {
+public
+class IntegerContext {
+ public:
+  IntegerContext(int bitlength) {
+    int numberOfBlocks = (bitlength + sizeof(uint64_t) - 1) / sizeof(uint64_t);
+    for (int i = 0; i < numberOfBlocks; i++) {
+      uint64_t modulus = maths::generatePrimes(15, sizeof(uint64_t));
+      modulus.push_back(modulus);
+    }
+  }
+
+  IntegerContext(const std::vector<uint64_t>& modulus) {
+    this.modulus = modulus;
+  }
+
+ private:
+  std::vector<uint64_t> modulus;
+};
+
+public
+class Integer {
+ private:
+  std::vector<uint64_t> values;
+  IntegerContext* ctx;
+};
+}  // namespace integer
